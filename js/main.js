cc48c33d35e7d90659686dcfe393c3b7f2bced17
ismailtaher <ismail.taher@ymail.com>
2023-10-05 18:50:36 +0500

    Third Commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 // Modules
 
-import playGuitar from "./guitars.js";
-import { shredding, plucking } from "./guitars.js";
+/* import playGuitar from "./guitars.js";
+import { shredding as shred, plucking as fingerPicking } from "./guitars.js"; */
 
-console.log(playGuitar());
-console.log(shredding());
-console.log(plucking());
+import * as Guitars from "./guitars.js";
+import User from "./user.js";
+const me = new User("email@email.com", "Ismail");
+console.log(me);
+console.log(me.greeting());
+console.log(Guitars.default());
+console.log(Guitars.shredding());
+console.log(Guitars.plucking());
