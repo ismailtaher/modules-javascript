613a7258a2cbf5a63e103e398122d7315819b8c5
ismailtaher <ismail.taher@ymail.com>
2023-10-05 17:25:29 +0500

    Second Commit

---
+++ js/guitars.js
@@ -0,0 +1,14 @@
+export default function playGuitar() {
+  return "Playing Guitar";
+}
+
+export const shredding = () => {
+  return "shredding some licks!";
+};
+
+export const plucking = () => {
+  return "Plucking the strings";
+};
+
+/* export default playGuitar;
+export { shredding, plucking }; */
--- js/main.js
+++ js/main.js
@@ -1,41 +1,8 @@
-// Web Storage API
+// Modules
 
-// Not a part of the DOM - refers to the Window API
-// Availabe in JS via the global variable: window
+import playGuitar from "./guitars.js";
+import { shredding, plucking } from "./guitars.js";
 
-// We don't have to type window. It is implied.
-
-/* window.alert(window.location);
-alert(location); */
-
-const myArray = ["eat", "sleep", "code"];
-const myObject = {
-  name: "Ismail",
-  hobbies: ["eat", "sleep", "code"],
-  logName: function () {
-    console.log(this.name);
-  },
-};
-
-// Session Storage
-
-/* sessionStorage.setItem("mySessionStore", JSON.stringify(myArray));
-const mySessionData = JSON.parse(sessionStorage.getItem("mySessionStore"));
-console.log(mySessionData); */
-
-// Local Storage
-
-localStorage.setItem("mylocalStore", JSON.stringify(myObject));
-// Removing a specific item or clearing out our local storage
-/* localStorage.removeItem("mylocalStore"); */
-/* localStorage.clear(); */
-const mylocalData = JSON.parse(localStorage.getItem("mylocalStore"));
-console.log(mylocalData);
-
-// Getting Key using Index
-const key = localStorage.key(0);
-console.log(key);
-
-// Getting length of our local Storage
-const length = localStorage.length;
-console.log(length);
+console.log(playGuitar());
+console.log(shredding());
+console.log(plucking());
